380237aeef5ce1ab060076aea340ff84736e26a7
Marcos <marcos.vinicios2@discente.ufg.br>
2023-09-27 03:32:09 -0300

    .

--- Hello.py
+++ Hello.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 from streamlit.logger import get_logger
 
@@ -20,6 +21,31 @@ def run():
               </style>
               """
   st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+  def gpt():
+    os.environ["OPENAI_API_KEY"] = "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"
+    openai_api_key = "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"
+
+    embeddings = OpenAIEmbeddings()
+    vectordb = Chroma(persist_directory = "./chroma_db", embedding_function = embeddings)
+    retriever = vectordb.as_retriever(search_type = "mmr")
+
+    msgs = StreamlitChatMessageHistory()
+
+    llm = ChatOpenAI(
+      model_name = "gpt-3.5-turbo",
+      temperature = 0,
+      streaming = True
+    )
+    memory = ConversationBufferMemory(memory_key = "chat_history", chat_memory = msgs, return_messages = True)
+
+    qa_chain = RetrievalQA.from_chain_type(
+      llm = llm,
+      chain_type = "stuff",
+      retriever = retriever,
+      memory = memory,
+      verbose = True,
+    )
 
 if __name__ == "__main__":
     run()
